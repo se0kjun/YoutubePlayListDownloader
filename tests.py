b6116dc5313f2f5b1cab4983ed14e171d016f936
SeokJun Hong <hong921122@gmail.com>
2015-11-22 23:21:26 +0900

    fix structure and add single video

--- tests.py
+++ tests.py
@@ -1,4 +1,7 @@
 import download
+from youtube_playlist_downloader.youtube_mp3_download import *
 
 if __name__ == '__main__':
-    print download.yt_download("https://www.youtube.com/playlist?list=PLkMRAPworbEffRLXVVAc-etlLIYGlPHvp")
+	print YoutubeSingleDownloader("https://www.youtube.com/watch?v=v2AVYvfNGfg").music_single_downloader()
+
+    # print download.yt_download("https://www.youtube.com/playlist?list=PLkMRAPworbEffRLXVVAc-etlLIYGlPHvp")
